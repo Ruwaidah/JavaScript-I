8b99c7d6ca7563a3ba26c069b57396791a7ff525
ruwaidah <Ruwidah.riyadh@gmail.com>
2019-09-16 17:41:13 -0500

    used filter method for car years

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -122,11 +122,13 @@ console.log(carYears);
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
 let oldCars = [];
-for (i = 0; i<length; i++){
-  if (inventory[i].car_year < 2000){
-    oldCars.push(inventory[i].car_year);
-  }
-}
+// for (i = 0; i<length; i++){
+//   if (inventory[i].car_year < 2000){
+//     oldCars.push(inventory[i].car_year);
+//   }
+// }
+
+oldCars = inventory.filter(word => word.car_year <2000);
 console.log(oldCars.length);
 
 // ==== Challenge 6 ====
